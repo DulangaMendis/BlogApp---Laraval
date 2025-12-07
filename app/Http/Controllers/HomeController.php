6183f9b87fdf5239e62b2d7551261a89b8eaa996
DulangaMendis <dulangamendis82@gmail.com>
2025-12-07 21:12:46 +0530

    show post data from data base in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
+
 class HomeController extends Controller
 {
     public function homepage()
     {
+        $post = Post::all();
         // return a view named "home"
-        return view('home.homepage');
+        return view('home.homepage',compact('post'));
     }
 }
